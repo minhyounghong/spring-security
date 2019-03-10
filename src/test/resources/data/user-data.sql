f3841106a6aac7f76bf2bd65038ba5aca3a07e55
kielhong <mh.hong@woowahan.com>
2019-03-10 09:56:21 +0900

    in test, disable auto init data.sql mode

--- src/test/resources/data/user-data.sql
+++ src/test/resources/data/user-data.sql
@@ -1,2 +1,2 @@
-INSERT INTO user (email, nickname, password)
+INSERT INTO user (id, email, nickname, password)
   VALUES ('user@bar.com', 'user', '$2a$10$4yjGFL/JYxRHgT2HZa913.hDzK/ZRb5EXRrOFWaRoUexO71sKO4sW');
